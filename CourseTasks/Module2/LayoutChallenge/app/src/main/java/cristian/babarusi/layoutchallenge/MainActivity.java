18f74708d36c98ca6945c2efad2459ba90c46fa8
babarusicristian <babarusi.cristian@gmail.com>
2019-02-04 19:32:40 +0200

    resolved all task for module 2

---
+++ CourseTasks/Module2/LayoutChallenge/app/src/main/java/cristian/babarusi/layoutchallenge/MainActivity.java
@@ -0,0 +1,13 @@
+package cristian.babarusi.layoutchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ CourseTasks/Module2/TravelJournal/app/src/main/java/cristian/babarusi/traveljournal/MainActivity.java
@@ -0,0 +1,33 @@
+package cristian.babarusi.traveljournal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButtonManageTrip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        mButtonManageTrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentManageTrip = new Intent(MainActivity.this, ManageTripActivity.class);
+                startActivity(intentManageTrip);
+            }
+        });
+    }
+
+    private void initViews() {
+        mButtonManageTrip = findViewById(R.id.button_manage_trip);
+    }
+}
