ce6aeeed17e82e495082f2358c186c28bb794393
babarusicristian <babarusi.cristian@gmail.com>
2019-03-03 02:35:02 +0200

    solved code challenge 4

--- CourseTasks/Module5/app/src/main/java/cristian/babarusi/module5/FirstChallenge4Activity.java
+++ CourseTasks/Module5/app/src/main/java/cristian/babarusi/module5/FirstChallenge4Activity.java
@@ -1,13 +1,72 @@
 package cristian.babarusi.module5;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FirstChallenge4Activity extends AppCompatActivity {
+
+    public static final String DATA_MESSAGE = "dataMessage";
+    public static final int CODE_KEY = 200;
+
+    private Button mButtonSend;
+    private EditText mEditTextMessage;
+    private TextView mTextViewDisplayMessage;
+    private TextView mTextViewReplyLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_challenge4);
+
+        initView();
+
+        //sending message to other activity
+        mButtonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mEditTextMessage.getText().toString().isEmpty()
+                || mEditTextMessage.getText().toString().equals("")) {
+                    mEditTextMessage.setError(getString(R.string.please_fill_a_message));
+                    mEditTextMessage.requestFocus();
+                } else {
+                    Intent intentAct2 = new Intent(FirstChallenge4Activity.this, SecondChallenge4Activity.class);
+                    intentAct2.putExtra(DATA_MESSAGE, mEditTextMessage.getText().toString());
+                    startActivityForResult(intentAct2, CODE_KEY);
+                }
+            }
+        });
+    }
+
+    private void initView() {
+        mButtonSend = findViewById(R.id.button_act1_send);
+        mEditTextMessage = findViewById(R.id.edittext_act1_message);
+        mTextViewDisplayMessage = findViewById(R.id.text_view_act1_display_message);
+        mTextViewReplyLabel = findViewById(R.id.text_view_reply_label);
+    }
+
+    //for receiving data from another intent and to display it
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CODE_KEY && resultCode == Activity.RESULT_OK) {
+
+            String mess = data.getStringExtra(SecondChallenge4Activity.DATA_MESSAGE);
+            mTextViewDisplayMessage.setText(mess);
+            mEditTextMessage.setText("");
+
+            mTextViewReplyLabel.setVisibility(View.VISIBLE);
+            mTextViewDisplayMessage.setVisibility(View.VISIBLE);
+
+        } else  if (resultCode == Activity.RESULT_CANCELED) {
+            //errors or something else
+        }
     }
 }
--- CourseTasks/Module5/app/src/main/java/cristian/babarusi/module5/SecondChallenge4Activity.java
+++ CourseTasks/Module5/app/src/main/java/cristian/babarusi/module5/SecondChallenge4Activity.java
@@ -1,13 +1,54 @@
 package cristian.babarusi.module5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondChallenge4Activity extends AppCompatActivity {
+
+    public static final String DATA_MESSAGE = "dataMessage";
+
+    private Button mButtonSend;
+    private EditText mEditTextMessage;
+    private TextView mTextViewDisplayMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_challenge4);
+
+        initView();
+
+        //receiving message from another activity
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            mTextViewDisplayMessage.setText(bundle.getString(FirstChallenge4Activity.DATA_MESSAGE));
+        }
+
+        //send back reply message and close this activity
+        mButtonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mEditTextMessage.getText().toString().isEmpty()
+                || mEditTextMessage.getText().toString().equals("")) {
+                    mEditTextMessage.setError(getString(R.string.please_fill_a_message));
+                } else {
+                    Intent intentAct1 = new Intent(SecondChallenge4Activity.this, FirstChallenge4Activity.class);
+                    intentAct1.putExtra(DATA_MESSAGE, mEditTextMessage.getText().toString());
+                    setResult(RESULT_OK, intentAct1);
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void initView() {
+        mButtonSend = findViewById(R.id.button_act2_send);
+        mEditTextMessage = findViewById(R.id.edittext_act2_message);
+        mTextViewDisplayMessage = findViewById(R.id.text_view_act2_display_message);
     }
 }
